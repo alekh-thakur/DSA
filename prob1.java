ea45a340ecdb6824ffbd7d40b4c078d487160eda
alekh-thakur <alekhrathor8@gmail.com>
2025-09-06 14:07:09 +0530

    Arrays : Problem 2

--- prob1.java
+++ prob1.java
@@ -1,6 +1,6 @@
 public class prob1{
     public static void main(String [] args){
-        int arr[] = {45,63,56,7,89,56,3};
+        int arr[] = {45,63,56,7,89,56,330};
         int result[] = min_max(arr);
         System.out.println("Maximum number is:" + result[0]);
         System.out.println("Minimum number is:" + result[1]);
@@ -10,7 +10,7 @@ public class prob1{
     static int[] min_max(int arr[]){
         int max=arr[0];
         int min = arr[0];
-        for(int i=0;i<arr.length-1;i++){
+        for(int i=0;i<arr.length;i++){
             if(arr[i]> max){
             max = arr[i];
             }
---
+++ prob2.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class prob2 {
+    public static void main(String[] args) {
+        int arr[] = {2,4,5,6,7,8};
+        int result[] = reverseArray(arr);
+        
+        for (int i : result) {
+            System.out.print(i+" ");
+        }
+    }
+
+    static int[] reverseArray(int arr[]){
+        int arr1[] = new int[arr.length];
+        int j=0;
+        for(int i=arr.length-1;i>=0;i--){
+            arr1[j] = arr[i];
+            j++;
+        }
+        return arr1;
+    }
+
+}
