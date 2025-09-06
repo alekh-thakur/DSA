124c3b5204a69ca31c5953156cb81ec795c7df83
alekh-thakur <alekhrathor8@gmail.com>
2025-09-06 13:30:45 +0530

    Arrays:Problem 1

---
+++ prob1.java
@@ -0,0 +1,24 @@
+public class prob1{
+    public static void main(String [] args){
+        int arr[] = {45,63,56,7,89,56,3};
+        int result[] = min_max(arr);
+        System.out.println("Maximum number is:" + result[0]);
+        System.out.println("Minimum number is:" + result[1]);
+
+    }
+
+    static int[] min_max(int arr[]){
+        int max=arr[0];
+        int min = arr[0];
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]> max){
+            max = arr[i];
+            }
+            if(arr[i]<min){
+                min =arr[i];
+            }
+        }
+        return new int[]{max,min};
+        
+    }
+}
